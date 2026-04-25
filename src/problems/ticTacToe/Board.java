ce1563fe933e01c4c71c70ad893d305dfedc5e8b
satwiksanand <sanandsatwik421@gmail.com>
2026-04-26 04:08:21 +0530

    add: fix print bug

--- src/problems/ticTacToe/Board.java
+++ src/problems/ticTacToe/Board.java
@@ -16,7 +16,7 @@ public class Board {
     public void printBoard() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(mat[i][j] + " ");
+                System.out.print(mat[i][j] == Symbol.EMPTY ? "#" : mat[i][j] + " ");
             }
             System.out.println();
         }
